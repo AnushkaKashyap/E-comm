eba89bbe99df1c7d160bc8f4840e4960c9cd9220
Anushka <84405298+AnushkaKashyap@users.noreply.github.com>
2026-08-25 15:05:57 +0530

    page uploaded

---
+++ script.js
@@ -0,0 +1,136 @@
+const triggerOpen = document.querySelectorAll('[trigger-button]');
+const triggerClose = document.querySelectorAll('[trigger-close]');
+
+const overlay = document.querySelector('[data-overlay]');
+
+for (let i = 0; i < triggerOpen.length; i++) {
+    let currentId =triggerOpen[i].dataset.target;
+    let targetEl = document.querySelector(`#${currentId}`);
+
+    const openData = function () { 
+        targetEl.classList.remove('active');
+        overlay.classList.remove('active');
+    };
+    triggerOpen[i].addEventListener('click', function () {
+        targetEl.classList.add('active');
+        overlay.classList.add('active');
+    });
+    targetEl.querySelector('[trigger-close]').addEventListener('click', openData);
+    overlay.addEventListener('click', openData);
+}
+
+//mobile-menu submenu
+const submenu = document.querySelectorAll('.child-trigger');
+submenu.forEach((menu) => menu.addEventListener('click', function (e) {
+    e.preventDefault();
+    submenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('active') : null);
+    
+    if (this.closest('.has-child').classList != 'active') {  
+        this.closest('.has-child').classList.toggle('active');
+    }
+}));
+
+//sorter
+const sorter = document.querySelector('.sort-list');
+if(sorter) {
+    const sortLi = sorter.querySelectorAll('li');
+    sorter.querySelector('.opt-trigger').addEventListener('click',function(){
+        sorter.querySelector('ul').classList.toggle('show');
+    });
+
+    sortLi.forEach((item) => item.addEventListener('click',function() {
+        sortLi.forEach((li)=> li != this? li.classList.remove('active') : null);
+        this.classList.add('active');
+        sorter.querySelector('.opt-trigger span.value').textContent = this.textContent;
+        sorter.querySelector('ul').classList.toggle('show');
+
+}))
+
+}
+
+//tabbed
+const trigger = document.querySelectorAll('.tabbed-trigger'), 
+    content = document.querySelectorAll('.tabbed> div ');
+trigger.forEach((btn) =>{
+    btn.addEventListener('click',function(){
+        let dataTarget = this.dataset.id,
+        body = document.querySelector(`#${dataTarget}`);
+
+        trigger.forEach((b)=> b.parentNode.classList.remove('active'));
+        trigger.forEach((s)=> s.classList.remove('active'));
+        content.forEach((c) => c.classList.remove('active')); //added while debugging - added via chatgpt
+        this.parentNode.classList.add('active');
+        body.classList.add('active');
+    })
+})  
+
+
+// Slider - Swiper JS
+const swiper = new Swiper('.sliderbox', {
+
+  loop: true,
+  effect: 'fade',
+  autoheight: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+// Carousel - Swiper JS
+const carousel = new Swiper('.carouselbox', {
+  spaceBetween: 30,
+  slidesPerView: 'auto',
+  centeredSlides: true,
+
+  // for navigation
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    481:{
+        slidesPerView: 2,
+        slidesPerGroup: 1,
+        centeredSlides: false,
+    },
+    640:{
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        centeredSlides: false,
+    },
+    880:{
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        centeredSlides: false,
+    },
+  } 
+});
+
+// Product Page - Product Image > Page-Single
+const thumbImage = new Swiper('.thumbnail-image', {
+
+  loop: true,
+  direction:'vertical',
+  spaceBetween : 15,
+  slidesPerView:1,
+  freeMode:true,
+  watchSlidesProgress:true,
+});
+
+const mainImage = new Swiper('.main-image', {
+
+//   loop: true,
+  autoheight:true,
+
+  pagination:{
+    el:'.swiper-pagination',
+    clickable:true,
+  },
+  thumbs:{
+    swiper: thumbImage,
+  },
+});
+
